fb88cb0a8a13c98d4a9c73ce0cc54aa801d8240c
kinsei <kinsei.musashi@gmail.com>
2015-02-26 06:12:15 -0900

    This is exercise 3. Basic math.

---
+++ ex3.py
@@ -0,0 +1,27 @@
+#this line prints out "I will now count my chickesn
+print "I will now count my chickens:"
+#these next two lines prints out how many hens and roosters you have and conducts the math to figure it out
+
+print "Hens", 25 + 30 / 6
+print "Roosters", 100 - 25 * 3 % 4
+#this line tells the user that it will now count the eggs
+
+print "Now I will coount the eggs"
+#this line has no strings to print out it strictly does the math and prints it out
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+#these next two lines asks a question and answers it    
+#it is preforming a boolean ie. true or false
+print "Is ti true that 3 + 2 < 5 - 7?"
+print 3 + 2 < 5 - 7 
+#this line askes a question and answers it
+print "What is 3 + 2 ", 3 + 2
+#this line asks a question and preformes the math to answer it
+print "What is 5 - 7?", 5 - 7 
+s line simply makes a statement
+print "Oh, thats why its false."
+#this line makes a statement
+print "how abut some more?"
+#the last three lines shows greater than, equal to or less than
+print "is it greater", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "is it less or equal?", 5 <= -2
