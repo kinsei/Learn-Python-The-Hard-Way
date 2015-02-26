7820e6f31a68e7e1b053a8decd4a2d8616dc5a36
kinsei <kinsei.musashi@gmail.com>
2015-02-25 21:14:01 -0900

    This is exercise 1

---
+++ ex1.py
@@ -0,0 +1,9 @@
+print "Hello World" #this prints a string
+print "hello again" #this also prints a string
+print "I like typing this" #this is a good exercise
+print "this is fun"   #i should start putting comments into my code
+print 'yay! printing' # i look forward to the next exercise
+print "I'd much rather you 'not'"  # so I should take this seriously
+print 'i "said" do not touch this'  #if you want to get better
+
+
