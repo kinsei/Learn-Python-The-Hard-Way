8665df1ee9380e5b8458a5be575f3a1f9ee2392d
kinsei <kinsei.musashi@gmail.com>
2015-02-26 19:45:38 -0900

    more sys

---
+++ ex17.py
@@ -0,0 +1,28 @@
+# Written for python 2.7
+# This is exercise 17 in Learn Pyton The Hard Way
+
+from sys import argv
+from os.path import exists
+
+script, from_file, to_file = argv
+
+print "Coppying from %s to %s" % (from_file, to_file)
+
+# We could do these two on one too, how?
+in_file = open(from_file)
+indata = in_file.read()
+
+print "The input fileis %s bytes long" % len(indata)
+
+print "Does the output file exist? %r" % exists(to_file)
+print "Ready, hit RETURN to continue. CTRL-C to abort."
+raw_input()
+
+out_file = open(to_file, 'w')
+out_file.write(indata)
+
+print "Alright, all done."
+
+out_file.close()
+in_file.close()
+
