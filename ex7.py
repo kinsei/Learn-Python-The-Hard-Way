23185128759f734b37fe32e70ffd3cbfef01e6d2
kinsei <kinsei.musashi@gmail.com>
2015-02-26 18:57:25 -0900

    exercise 7 adding strs

---
+++ ex7.py
@@ -0,0 +1,23 @@
+print "Marry had a little lam"   #this program prints out a joke to the moniter
+print "Its fleece was white as %s." % 'snow' 
+print "And everywhere that Marry went."
+print "." * 10 # what did that do? this printed out ten . to the moniter
+
+end1 = "c"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+#watch that comma at the end. try removing it to see what happens
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 + end12
+
+#removing the , from end prints two seperat lines insted of one
