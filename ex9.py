c92c6133643bb4f8cddff9900cf9e075634d2420
kinsei <kinsei.musashi@gmail.com>
2015-02-26 19:39:18 -0900

    exercise 9

---
+++ ex9.py
@@ -0,0 +1,16 @@
+# Here's some new strange stuff, rember to type it exactly
+
+days = "Mon Tue Wed Thr Fri Sat Sun"
+months = "Jan\nFeb\nMar\nArp\nMay\nJun\nJul\nAug"
+
+print "Here are the days:", days
+print "Here are the months:", months
+
+print """
+There's something going on here.
+With the three duble-quotes.
+We'll be able to type as much as we like.
+
+C
+Even $ lines if we wanted, or 5,
+"""
