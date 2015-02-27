000fbe916b683eead671502439de8ba11f3a23c2
kinsei <kinsei.musashi@gmail.com>
2015-02-26 19:41:53 -0900

    more raw_input

---
+++ ex12.py
@@ -0,0 +1,8 @@
+# Exercise 12 of Learn Python The Hard Way
+# Written for python 2.7
+
+age = raw_input("How old are you")
+hight = raw_input("Howtall are you")
+weight = raw_input("How much do you weigh?")
+
+print "So,you're %r old, %r tall and %r heavy." % (age, hight, weight)
