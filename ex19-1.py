41e0c65f7358f6ed9dc965237d98cd9ca699ab60
kinsei <kinsei.musashi@gmail.com>
2015-03-02 09:38:55 -0900

    exercise 19

---
+++ ex19-1.py
@@ -0,0 +1,11 @@
+# This was written for python 2.7
+# This is a part of Learning Python The Hard Way exercise 19
+
+# The pourpose of this exercise is is to write a function and run it 10 different ways
+
+start_number = int(raw_input("Enter PC NUM:"))
+
+def add_number(start_number):
+    print start_number + 1
+
+add_number(start_number)
