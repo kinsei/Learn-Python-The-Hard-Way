a90d37010311826abd3a6fb9b06261a4398d40d1
kinsei <kinsei.musashi@gmail.com>
2015-03-02 14:29:56 -0900

    Exercise 31

---
+++ ex31.py
@@ -0,0 +1,15 @@
+print "You enter a dark room with two doors. Do you go through door #1 or 
+       door #2?"
+
+door = raw_input(">")
+
+if door == "1":
+    print "There's a giant bear here eatinga cheese cake."
+    print "1. Take the cake"
+    print "2. Scream at the bear."
+
+    bear = raw_input(">")
+
+    if bear == "1":
+        print "The bear eats your face off"
+
