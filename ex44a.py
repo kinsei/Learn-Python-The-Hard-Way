6c8cfdde3bdb1fafd92bc5a8132e2a34b2170496
root <root@localhost.localdomain>
2015-05-30 22:42:36 -0800

    exercise 44

---
+++ ex44a.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+#inheritance vs. Composition
+
+class Parent(object):
+    
+    def implicit(self):
+        print "Parent implicit()"
+
+class Child(Parent):
+
+    pass 
+
+dad = Parent()
+son = Child()
+
+dad.implicit()
+son.implicit()
+
