4d44b9f8822050983a7445511e9e2f73b1cdf512
root <root@localhost.localdomain>
2015-05-30 22:42:59 -0800

    exercise 44

---
+++ ex44e.py
@@ -0,0 +1,33 @@
+# Composition
+
+class Other(object):
+    
+    def override(self):
+        print "Other override()"
+
+    def implicit(self):
+        print "Other implicit()"
+
+    def altered(self):
+        print "Other altered"
+
+class Child(object):
+
+    def __init__(self):
+        self.other = Other()
+
+    def implicit(self):
+        self.other.implicit()
+
+    def override(self):
+        print "Child override()"
+
+    def altered(self):
+        print "Child, before altered()"
+        self.other.altered()
+        print "Child after altered()"
+
+son = Child()
+son.implicit()
+son.override()
+son.altered()
